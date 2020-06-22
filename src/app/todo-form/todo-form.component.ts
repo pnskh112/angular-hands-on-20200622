918543e8c4d390715e0f393fe9d12c0c13a9547d
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-06-22 20:29:46 +0900

    7 files modified, 3 added

---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent implements OnInit {
+  title: string;
+
+   @Output() add = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  create() {
+    this.add.emit(this.title);
+    this.title = '';
+  }
+}
